e2e37dcfa2eb2af9f02bf55fd3d372d45eb01686
shubham99177 <shubhamrathoree123@gmail.com>
2023-12-14 16:52:26 +0530

    first

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 // import { Express } from 'express'           //both are same
 const app = express()
-const port = 3001
+const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -23,3 +23,4 @@ app.get('/twiter', (req, res) => {
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
